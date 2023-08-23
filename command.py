0868deb49c1d663298c1c2865e545708d887f0ba
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-08-23 16:44:47 +0300

    Update command.py

--- command.py
+++ command.py
@@ -1,38 +1,25 @@
 global command_word
 
 telephone_list = {"Olha":"+380996409040"}
+
+def catch_error(text_error, func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except IndexError:
+        return text_error
 #decorator
 def input_error(func):
-    def inner(list_text):
+    def inner(*args, **kwargs):
+        list_text = args[0]  
         parser_list = list_text.split()
         if len(parser_list) == 3:
-            try:
-                result = func(list_text)
-            except IndexError:
-                result = "Invalid input format. Give me name and phone please." 
-                print(result)     
-            return result        
+            return catch_error("Invalid input format. Give me name and phone please.", func, *args, **kwargs)
         if len(parser_list) == 2:
-            try:
-                result = func(list_text)
-            except IndexError:
-                result = "Invalid input format. Give me phone please."   
-                print(result)    
-            return result
+            return catch_error("Invalid input format. Give me phone please.", func, *args, **kwargs)
         if len(parser_list) == 1 and parser_list[0] == "phone":
-            try:
-                result = func(list_text)
-            except IndexError:
-                result = "Invalid input format. Give me phone please."   
-                print(result)    
-            return result
+            return catch_error("Invalid input format. Give me phone please.", func, *args, **kwargs)
         if len(parser_list) == 1 and parser_list[0] == "change":
-            try:
-                result = func(list_text)
-            except IndexError:
-                result = "Invalid input format. Give me phone please."   
-                print(result)    
-            return result
+            return catch_error("Invalid input format. Give me phone please.", func, *args, **kwargs)    
     return inner
 
 # main functions
@@ -55,40 +42,37 @@ def say_good_bye():
 @input_error
 def add_new_telephone_contact(text):    
     text_list = text.split()    
-    telephone_list.update({text_list[1].title():text_list[2]})
-    print(f"The contact {text_list[1].title()} has been successfully added")
+    telephone_list.update({text_list[1].title():text_list[2]})    
     
 @input_error
 def change_phone_number(text):
     text_list = text.split()
-    telephone_list[text_list[1].title()] = text_list[2]
-    print(f"Phone {text_list[1].title()} successfully changed to {text_list[2]}")
+    telephone_list[text_list[1].title()] = text_list[2] 
 
 @input_error
 def show_name_contact(text):
     text_list = text.split()
     for key, value in telephone_list.items():
         if value == text_list[1]:
-            print(key)
             return key
 
 def bot_operation(text):
     match text:
         case "hello":
-            result = hello()
-            print(result)
+            result = hello()   
+                     
         case "close":
-            result = say_good_bye()
-            print(result)
+            result = say_good_bye()  
+
         case "good bye":
-            result = say_good_bye()
-            print(result)
+            result = say_good_bye()  
+
         case "exit":
-            result = say_good_bye()
-            print(result)
+            result = say_good_bye()   
+
         case "show all":
             result = show_all()
-            print(result)
+            
         case "add":
             result = add_new_telephone_contact(command_word)
             
