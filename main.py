a672fa24e836b4afe3146f179109a2a00772c224
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-08-23 16:44:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def main():
     while command.command_word not in stop_words:
         command.command_word = input("Enter the command: ").lower() # The bot is not sensitive to the case of the entered commands
         temp_run = command_recognizer(command.command_word)
-        for i in range(len(temp_run)):            
-            bot_operation(temp_run[i])
+        for i in range(len(temp_run)):
+            if bot_operation(temp_run[i]) != None:
+                print(bot_operation(temp_run[i]))
+            else:
+                continue
 
 
 if __name__ == "__main__":
